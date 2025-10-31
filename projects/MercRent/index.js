6ff702e8652d2a59ca15a1bd29ede24a3d239053
SamPM092 <sammich4294@gmail.com>
2025-10-31 17:37:32 +0700

    feat: fighters cards

--- projects/MercRent/index.js
+++ projects/MercRent/index.js
@@ -14,3 +14,18 @@ window.addEventListener('scroll', () => { //menghilangkan header ketika scroll
 
     lastScrollY = currentScrollY;
 });
+
+// Find all cards
+const cards = document.querySelectorAll('.card');
+
+// Loop through each card and fill in data
+cards.forEach(card => {
+    const data = JSON.parse(card.getAttribute('data-card'));
+    console.log(data);
+
+    card.querySelector('.name').textContent = data.name;
+    card.querySelector('.duration').textContent = data.duration;
+    card.querySelector('.price').textContent = data.price;
+    card.querySelector('.rating').textContent = data.rating;
+    card.querySelector('.member').textContent = data.member;
+}); 
